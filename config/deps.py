3d92b8601ae5feccb867bf9748d4b76372144a9a
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-05 16:18:50 +0300

    fixing build

---
+++ config/deps.py
@@ -0,0 +1,4 @@
+packages=[
+    'npm',
+    'nodejs',
+]
---
+++ config/project.py
@@ -0,0 +1,4 @@
+project_name = "demos-highlight"
+project_long_description = "Demos for doing text highlighting"
+project_year_started = 2020
+project_description = """Demos for doing text highlighting"""
