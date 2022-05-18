4fe16701979a03e20f3c434594c9f7b0e42f431b
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:55 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-highlight"
-project_long_description = "Demos for doing text highlighting"
-project_year_started = 2020
-project_description = """Demos for doing text highlighting"""
+description_short = "Demos for doing text highlighting"
