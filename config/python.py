4aca28ea671f834d056f34ea7291520cbdc99695
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 13:47:44 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,10 +1,10 @@
 """ python deps for this project """
 
+import config.shared
+
 install_requires: list[str] = [
     "pillow",
 ]
-build_requires: list[str] = [
-    "pymakehelper",
-    "pydmt",
-]
-requires = install_requires + build_requires
+build_requires: list[str] = config.shared.BUILD
+test_requires: list[str] = config.shared.TEST
+requires = install_requires + build_requires + test_requires
